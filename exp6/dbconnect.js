ea9e773e94c5fedb656767a1df9ee017c0a86932
gudlavalleru2022 <gudlavalleruenggclg@gmial.com>
2025-03-21 10:20:45 +0530

    exp6

---
+++ exp6/dbconnect.js
@@ -0,0 +1,7 @@
+var mongoose=require("mongoose");
+
+mongoose.set("strictQuery",false);
+
+mongoose.connect("mongodb://127.0.0.1:27017/demo")
+    .then(() =>console.log("Database connected"))
+    .catch((Error) =>console.log(Error));
